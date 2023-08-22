7d68e732b8c620917c8ffbf57cc1d41e4258656f
Medha Praneeth Reddy Modulla <medha.modulla@gmail.com>
2023-08-22 10:29:14 -0400

    New CSS and JS files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import searchIcon from './search.svg';
+
+//apiKey : 694d97c2
+const API_URL = 'http://omdbapi.com?apikey=694d97c2';
+
+const App = () => {
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+
+    return(
+        <h1>App</h1>
+    )
 }
 
 export default App;
